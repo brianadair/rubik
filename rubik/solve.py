ef9b86eda9b163ec87988d58e350fd242caefb3f
Brian <Brian@MOONSHINE>
2022-02-24 18:33:09 -0600

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -2,5 +2,8 @@ import rubik.cube as cube
 
 def _solve(parms):
     result = {}
-    encodedCube = parms.get('cube',None)       #get "cube" parameter if present
+    cube_model = cube.Cube(parms)
+    if cube_model._isValidCube():
+        encodedCube = parms.get('cube',None)       #get "cube" parameter if present
+        result['status'] = 'ok'
     return result
