bfbbd8fdb946fbf064363bf1773e71a7c7707f47
Brian <Brian@MOONSHINE>
2022-02-27 19:15:27 -0600

    Green Light |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -85,7 +85,7 @@ class Test(unittest.TestCase):
 # Sad path tests
 
     def test_910_ShouldErrorOnMissingConstructorArgument(self):
-        expectedResult = type(TypeError)
+        expectedResult = '<class \'TypeError\'>'
         try:
             test_cube = Cube()
             actualResult = type(test_cube.getCube())
