d1372bc0f2feb4996b3417dcd7c4fdf271d4c683
Brian <Brian@MOONSHINE>
2022-02-28 22:06:27 -0600

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -4,7 +4,11 @@ import rubik.cube as cube
 
 def _solve(parms):
     result = {}
-    cube_model = cube.Cube(parms)
+    try:
+        cube_model = cube.Cube(parms)
+    except AttributeError:
+        result['status'] = 'error: no cube argument provided'
+        return result    
     
     if not cube_model._isValidCube():
         #encodedCube = parms.get('cube',None)       #get "cube" parameter if present
