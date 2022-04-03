eefe4015f4b261a2a8d5fab209e3e7becb52904c
Brian <Brian@MOONSHINE>
2022-04-03 18:48:01 -0500

    Refactor |

--- rubik/cube.py
+++ rubik/cube.py
@@ -272,8 +272,8 @@ class Cube:
         while self._isFlippedTopEdgePhaseOne():
             for key in keys:
                 face = math.floor(edge / 9)
-                print(f"Edge is {edge}, face is {face}")
-                print(f"Cube {self.cube_state[edge-1]}")
+                #print(f"Edge is {edge}, face is {face}")
+                #print(f"Cube {self.cube_state[edge-1]}")
                 if (self.cube_state[edge-1] == bottomMid):
                     #face = math.floor(edge / 9) - 1
                     left = face - 1
