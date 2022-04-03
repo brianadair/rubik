a008f7f16671386cc8cf0fa024b29c976f97391e
Brian <Brian@MOONSHINE>
2022-04-03 18:33:49 -0500

    Refactor |

--- rubik/cube.py
+++ rubik/cube.py
@@ -272,6 +272,7 @@ class Cube:
             for key in keys:
                 face = math.floor(edge / 9)
                 print(f"Edge is {edge}, face is {face}")
+                print(f"Cube {self.cube_state}")
                 if (self.cube_state[edge-1] == bottomMid):
                     #face = math.floor(edge / 9) - 1
                     left = face - 1
