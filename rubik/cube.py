ce34f55f08241acc2ba8941027f28bf4c96daddf
Brian <Brian@MOONSHINE>
2022-04-04 18:57:36 -0500

    Green Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -531,7 +531,7 @@ class Cube:
         for sqr in bottomFace: # real numbers of bottom face
             if count in self.faceCorners:   # is this square a corner
                 if self.cube_state[sqr-1] == self._getMiddleColor(sqr-1): #matches its own face middle then check:
-                    sqrAdjList = bottomAdjDict.get(sqr - 1)
+                    sqrAdjList = self.bottomAdjDict.get(sqr - 1)
                     for adj in sqrAdjList:  # real array nums
                         face = math.floor(adj / 9)     #determine face that sqr belongs to
                         faceMid = (face * self.faceIncrement) + self.midIncrement # get middle color for that face
