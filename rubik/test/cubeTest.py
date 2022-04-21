32d40d5c580cbb667e46d1f4a3ba460efdce65a2
Brian <Brian@MOONSHINE>
2022-04-20 20:39:47 -0500

    Refactor |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -361,7 +361,7 @@ class Test(unittest.TestCase):
                 #'cube': 'ggobggogbwgbororryryrybyobbyrrrooooggbwbyrwygywywwwwwb'}#cube2 false
                 'cube':'bgoggboggggbororryryrybyobbyrrroooowgbwbyrywygywwwwwwb'} #cube above after FF move
         test_cube = Cube(parm)
-        actualResult = test_cube._isEdgeMismatchedForMiddleLayer(0,['5','12'])
+        actualResult = test_cube._isEdgeMismatchedForMiddleLayer(0,[5,12])
         print(f"9903 Edge mismatch?: {actualResult}")
 
     def test_09904_ShouldReturnTrueOnMatchedRightEdgeForMiddleLayer(self):
